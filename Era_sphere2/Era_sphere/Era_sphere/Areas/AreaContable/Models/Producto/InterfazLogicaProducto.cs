2c4ea0f7bb6458a17f28f5c13f33598f95f71878
fhuamanm@pucp.pe <fhuamanm@pucp.pe>
2012-05-28 18:30:40 +0000

    Agregado TODO para Administrar Productos

---
+++ Era_sphere2/Era_sphere/Era_sphere/Areas/AreaContable/Models/Producto/InterfazLogicaProducto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Era_sphere.Areas.AreaContable.Models.Producto
+{
+    public interface InterfazLogicaProducto
+    {
+        void agregarProducto(ProductoView producto);
+        void modificarProducto(ProductoView producto);
+        void eliminarProducto(int producto_id);
+        List<ProductoView> retornarProductos();
+        ProductoView retornarProducto(int hotel_id);
+    }
+}
---
+++ Era_sphere2/Era_sphere/Era_sphere/Areas/AreaContable/Models/Producto/ProductoContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Era_sphere.Areas.AreaContable.Models.Producto
+{
+    public class ProductoContext
+    {
+        public DbSet<Producto> productos { get; set; }
+    }
+}

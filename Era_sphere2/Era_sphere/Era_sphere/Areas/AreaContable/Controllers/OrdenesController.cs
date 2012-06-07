0bf4374c0f40f4ea642badd2473a8bf4144c0535
lengua.carlos@pucp.pe <lengua.carlos@pucp.pe>
2012-06-07 21:17:23 +0000

    Mantenimiento de Ordenes al 90%

--- Era_sphere2/Era_sphere/Era_sphere/Areas/AreaContable/Controllers/OrdenesController.cs
+++ Era_sphere2/Era_sphere/Era_sphere/Areas/AreaContable/Controllers/OrdenesController.cs
@@ -33,59 +33,82 @@ namespace Era_sphere.Areas.AreaContable.Controllers
         {
             List<Orden> os = db.ordenes.ToList();
             List<OrdenView> ovs = new List<OrdenView>();
-            foreach (Orden o in os) ovs.Add(new OrdenView(o));
+            foreach (Orden o in os)
+            {
+                o.total = 0;
+                o.nro_lineas = 0;
+                foreach (OrdenLinea ol in o.ordenlineas)
+                {
+                    o.total += ol.SubTotal;
+                    o.nro_lineas++;
+                }
+                ovs.Add(new OrdenView(o));
+            }
             return View("IndexOrdenes", new GridModel(ovs));
         }
         [AcceptVerbs(HttpVerbs.Post)]
         [GridAction]
-        public ActionResult Insert()
+        public ActionResult Update(OrdenView ov)
         {
+            Orden ord = db.ordenes.Find(ov.ID);
+            ord.estado = ov.estadoID;
+            db.Entry(ord).State = EntityState.Modified;
+            db.SaveChanges();
 
-            OrdenView ov = new OrdenView();
-            if (TryUpdateModel(ov))
+            /**/
+            List<Orden> os = db.ordenes.ToList();
+            List<OrdenView> ovs = new List<OrdenView>();
+            foreach (Orden o in os)
             {
-                Orden o = ov.deserealizar();
-                db.ordenes.Add(o);
-                db.SaveChanges();
+                o.total = 0;
+                o.nro_lineas = 0;
+                foreach (OrdenLinea ol in o.ordenlineas)
+                {
+                    o.total += ol.SubTotal;
+                    o.nro_lineas++;
+                }
+                ovs.Add(new OrdenView(o));
             }
-            return View("IndexOrdenes", new GridModel(db.ordenes.ToList()));
+            return View("IndexOrdenes", new GridModel(ovs));
         }
+
         [AcceptVerbs(HttpVerbs.Post)]
         [GridAction]
         public ActionResult Delete(int? id)
         {
             db.ordenes.Remove(db.ordenes.Find(id));
             db.SaveChanges();
-            return View("Index", new GridModel(db.ordenes.ToList()));
-        }
-        [AcceptVerbs(HttpVerbs.Post)]
-        [GridAction]
-        public ActionResult Update(OrdenView ov)
-        {
-            Orden o = ov.deserealizar();
-            db.Entry(o).State = EntityState.Modified;
-            db.SaveChanges();
-            return View("Index", new GridModel(db.ordenes.ToList()));
+
+            List<Orden> os = db.ordenes.ToList();
+            List<OrdenView> ovs = new List<OrdenView>();
+            foreach (Orden o in os) ovs.Add(new OrdenView(o));
+            return View("IndexOrdenes", new GridModel(ovs));
+            //return View("Index", new GridModel(db.ordenes.ToList()));
         }
 
-        public ActionResult agregar()
+        public ActionResult agregar(int? oid)
         {
-            Orden o = new Orden
+            if (oid == null)
             {
-                estado = 1,
-                fechaRegistro = DateTime.Today,
-                totalIGV = 0,
-                total = 0,
-                ordenlineas = new List<OrdenLinea>()
-            };
-            db.ordenes.Add(o);
-            db.SaveChanges();
-
-            ViewData["oid"] = o.ID;
+                Orden o = new Orden
+                {
+                    estado = 0,
+                    fechaRegistro = DateTime.Today,
+                    totalIGV = 0,
+                    total = 0,
+                    ordenlineas = new List<OrdenLinea>()
+                };
+                db.ordenes.Add(o);
+                db.SaveChanges();
+                ViewData["oid"] = o.ID;
+            }
+            else
+                ViewData["oid"] = oid;
 
             return View("AgregarOrden");
         }
-
+        [AcceptVerbs(HttpVerbs.Post)]
+        [GridAction]
         public ActionResult SelectLineas(int oid)
         {
             ICollection<OrdenLineaView> olvs = new List<OrdenLineaView>();
@@ -95,7 +118,9 @@ namespace Era_sphere.Areas.AreaContable.Controllers
             ViewData["oid"] = oid;
             return View("AgregarOrden", new GridModel(olvs));
         }
+        [AcceptVerbs(HttpVerbs.Post)]
 
+        [GridAction]
         public ActionResult InsertLinea(int oid)
         {
             OrdenLineaView olv = new OrdenLineaView();
@@ -106,6 +131,20 @@ namespace Era_sphere.Areas.AreaContable.Controllers
                 db.ordeneslineas.Add(o);
                 db.SaveChanges();
             }
+
+            ICollection<OrdenLineaView> olvs = new List<OrdenLineaView>();
+            ICollection<OrdenLinea> ols = db.ordenes.Find(oid).ordenlineas;
+            foreach (OrdenLinea ol in ols) olvs.Add(new OrdenLineaView(ol));
+
+            ViewData["oid"] = oid;
+            return View("AgregarOrden", new GridModel(olvs));
+        }
+        [AcceptVerbs(HttpVerbs.Post)]
+        [GridAction]
+        public ActionResult DeleteLinea(int? id, int? oid)
+        {
+            db.ordeneslineas.Remove(db.ordeneslineas.Find(id));
+            db.SaveChanges();
 
             ICollection<OrdenLineaView> olvs = new List<OrdenLineaView>();
             ICollection<OrdenLinea> ols = db.ordenes.Find(oid).ordenlineas;
@@ -137,109 +176,3 @@ namespace Era_sphere.Areas.AreaContable.Controllers
 
     }
 }
-
-
-
-
-            //if (db.ordenes.ToList().Count == 0) ViewData["oid"] = 0;
-            //else ViewData["oid"] = db.ordenes.OrderByDescending(o => o.ID).FirstOrDefault().ID + 1;
-
-
-
-
-
-        //public ViewResult Index()
-        //{
-        //    var ordenes = db.ordenes.Include(o => o.empleado_solicita);
-        //    return View(ordenes.ToList());
-        //}
-
-        ////
-        //// GET: /AreaContable/Ordenes/Details/5
-
-        //public ViewResult Details(int? id)
-        //{
-        //    Orden orden = db.ordenes.Find(id);
-        //    return View(orden);
-        //}
-
-        ////
-        //// GET: /AreaContable/Ordenes/Create
-
-        //public ActionResult Create()
-        //{
-        //    ViewBag.empleado_solicitaID = new SelectList(db.empleados, "ID", "descripcion");
-        //    return View();
-        //} 
-
-        ////
-        //// POST: /AreaContable/Ordenes/Create
-
-        //[HttpPost]
-        //public ActionResult Create(Orden orden)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.ordenes.Add(orden);
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");  
-        //    }
-
-        //    ViewBag.empleado_solicitaID = new SelectList(db.empleados, "ID", "descripcion", orden.empleado_solicitaID);
-        //    return View(orden);
-        //}
-        
-        ////
-        //// GET: /AreaContable/Ordenes/Edit/5
- 
-        //public ActionResult Edit(int id)
-        //{
-        //    Orden orden = db.ordenes.Find(id);
-        //    ViewBag.empleado_solicitaID = new SelectList(db.empleados, "ID", "descripcion", orden.empleado_solicitaID);
-        //    return View(orden);
-        //}
-
-        ////
-        //// POST: /AreaContable/Ordenes/Edit/5
-
-        //[HttpPost]
-        //public ActionResult Edit(Orden orden)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Entry(orden).State = EntityState.Modified;
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
-        //    ViewBag.empleado_solicitaID = new SelectList(db.empleados, "ID", "descripcion", orden.empleado_solicitaID);
-        //    return View(orden);
-        //}
-
-        ////
-        //// GET: /AreaContable/Ordenes/Delete/5
- 
-        //public ActionResult Delete(int id)
-        //{
-        //    Orden orden = db.ordenes.Find(id);
-        //    return View(orden);
-        //}
-
-        ////
-        //// POST: /AreaContable/Ordenes/Delete/5
-
-        //[HttpPost, ActionName("Delete")]
-        //public ActionResult DeleteConfirmed(int id)
-        //{            
-        //    Orden orden = db.ordenes.Find(id);
-        //    db.ordenes.Remove(orden);
-        //    db.SaveChanges();
-        //    return RedirectToAction("Index");
-        //}
-
-        //protected override void Dispose(bool disposing)
-        //{
-        //    db.Dispose();
-        //    base.Dispose(disposing);
-        //}
-//    }
-//}

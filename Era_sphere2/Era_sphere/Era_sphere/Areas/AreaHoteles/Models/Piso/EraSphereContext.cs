4b277c692a1f1eaf8eb5714b222e2990c60c126f
lengua.carlos@pucp.pe <lengua.carlos@pucp.pe>
2012-06-24 16:22:29 +0000

    Codigo pisos a 3 digitos

--- Era_sphere2/Era_sphere/Era_sphere/Areas/AreaHoteles/Models/Piso/EraSphereContext.cs
+++ Era_sphere2/Era_sphere/Era_sphere/Areas/AreaHoteles/Models/Piso/EraSphereContext.cs
@@ -15,13 +15,13 @@ namespace Era_sphere.Generics
         {
             List<Piso> ps = new List<Piso>()
             {
-                new Piso(){descripcion="Primer piso",codigo_piso ="P01",hotelID=1, hotel = hoteles.Find(1)},
-                new Piso(){descripcion="Segundo piso",codigo_piso ="P02",hotelID=1, hotel = hoteles.Find(1)},
-                new Piso(){descripcion="Tercer piso",codigo_piso ="P03",hotelID=1, hotel = hoteles.Find(1)},
-                new Piso(){descripcion="Cuarto piso",codigo_piso ="P04",hotelID=1, hotel = hoteles.Find(1)},
-                new Piso(){descripcion="Primer piso",codigo_piso ="P01",hotelID=2, hotel = hoteles.Find(2)},
-                new Piso(){descripcion="Segundo piso",codigo_piso ="P02",hotelID=2, hotel = hoteles.Find(2)},
-                new Piso(){descripcion="Tercer piso",codigo_piso ="P03",hotelID=2, hotel = hoteles.Find(2)},
+                new Piso(){descripcion="Piso 001",codigo_piso ="P001",hotelID=1, hotel = hoteles.Find(1)},
+                new Piso(){descripcion="Piso 002",codigo_piso ="P002",hotelID=1, hotel = hoteles.Find(1)},
+                new Piso(){descripcion="Piso 003",codigo_piso ="P003",hotelID=1, hotel = hoteles.Find(1)},
+                new Piso(){descripcion="Piso 004",codigo_piso ="P004",hotelID=1, hotel = hoteles.Find(1)},
+                new Piso(){descripcion="Piso 001",codigo_piso ="P001",hotelID=2, hotel = hoteles.Find(2)},
+                new Piso(){descripcion="Piso 002",codigo_piso ="P002",hotelID=2, hotel = hoteles.Find(2)},
+                new Piso(){descripcion="Piso 003",codigo_piso ="P003",hotelID=2, hotel = hoteles.Find(2)},
             };
             foreach (Piso p in ps) pisos.Add(p);
             SaveChanges();
